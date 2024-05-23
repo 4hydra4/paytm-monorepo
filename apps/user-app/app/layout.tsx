e881843df3cd9f112e29acc8bbce33b56eb7ec5a
Danish <danish4work@gmail.com>
2024-05-23 14:31:19 +0530

    approve

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -9,6 +9,7 @@ import { AppbarClient } from "../components/AppbarClient";
 const inter = Inter({ subsets: ["latin"] });
 
 
+
 export const metadata: Metadata = {
   title: "Wallet",
   description: "Simple wallet app",
