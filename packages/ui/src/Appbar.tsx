55f0e04d03980971b056367b871b639f0562e1a2
Danish <danish4work@gmail.com>
2024-05-27 23:49:37 +0530

    added logged in user's name to appbar display

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -1,25 +1,28 @@
+"use client";
 import { Button } from "./button";
 
 interface AppbarProps {
-    user?: {
-        name?: string | null;
-    },
-    // TODO: can u figure out what the type should be here?
-    onSignin: any,
-    onSignout: any
+  user?: {
+    name?: string | null;
+  };
+  // TODO: can u figure out what the type should be here?
+  onSignin: any;
+  onSignout: any;
 }
 
-export const Appbar = ({
-    user,
-    onSignin,
-    onSignout
-}: AppbarProps) => {
-    return <div className="flex justify-between border-b px-4 border-slate-300">
-        <div className="text-lg flex flex-col justify-center">
-            PayTM
+export const Appbar = ({ user, onSignin, onSignout }: AppbarProps) => {
+  return (
+    <div className="flex justify-between border-b px-4 border-slate-300">
+      <div className="text-xl flex flex-col justify-center">PayTM</div>
+
+      <div className="flex">
+        <div className="text-lg text-[#603ead] flex flex-col justify-center mr-4">Hello, {user?.name}!</div>
+        <div className="pt-2">
+          <Button onClick={user ? onSignout : onSignin}>
+            {user ? "Logout" : "Login"}
+          </Button>
         </div>
-        <div className="flex flex-col justify-center pt-2">
-            <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
-        </div>
+      </div>
     </div>
-}
+  );
+};
